81a706c676d5c598f96096ca1c503409097b6cc5
Takayama <49364055+MGMCN@users.noreply.github.com>
2023-02-25 22:03:50 +0900

    update project

---
+++ bilibili/bilibili/spiders/bilibilicomment.py
@@ -0,0 +1,32 @@
+import scrapy
+from urllib.parse import urlencode
+from bilibili.items import BilibiliItem
+
+
+class BilibilicommentSpider(scrapy.Spider):
+    name = 'bilibilicomment'
+    allowed_domains = ['api.bilibili.com']
+    api_url = 'https://api.bilibili.com/pgc/review/short/list?'
+    params = {
+        'media_id': '28338491',
+        'ps': '20',
+        'sort': '0',
+    }
+    start_urls = [api_url + urlencode(params)]
+
+    def parse(self, response):
+        json = response.json()
+        if json:
+            datas = json.get('data')
+            items = datas.get('list', {})
+            next = datas.get('next')
+            if next:
+                self.params['cursor'] = str(next)
+                next_url = self.api_url + urlencode(self.params)
+                yield scrapy.Request(url=next_url, callback=self.parse)
+            if items:
+                if len(items) > 0:
+                    for item in items:
+                        bilibiliitem = BilibiliItem()
+                        bilibiliitem['comment'] = item.get('content')
+                        yield bilibiliitem
